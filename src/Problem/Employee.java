b1d50a5a9c3f0a7f3e5b6c6f578c4c919f57a235
Yann Derré <yann.derre44@gmail.com>
2023-06-09 19:42:44 +0200

    started making it compatible

--- src/Problem/Employee.java
+++ src/Problem/Employee.java
@@ -1,10 +1,49 @@
 package Problem;
 
 public class Employee {
-    
     public int id;
     public int center_id;
-
     public String competence;
     public String specialite;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", centerId=" + center_id +
+                ", skill='" + competence + '\'' +
+                ", specialty='" + specialite + '\'' +
+                '}';
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCenter_id() {
+        return center_id;
+    }
+
+    public void setCenter_id(int center_id) {
+        this.center_id = center_id;
+    }
+
+    public String getCompetence() {
+        return competence;
+    }
+
+    public void setCompetence(String competence) {
+        this.competence = competence;
+    }
+
+    public String getSpecialite() {
+        return specialite;
+    }
+
+    public void setSpecialite(String specialite) {
+        this.specialite = specialite;
+    }
 }
---
+++ src/Problem/distanceMatrix.java
@@ -0,0 +1,19 @@
+package Problem;
+
+public class distanceMatrix{
+    private Float[][] distances;
+
+    public distanceMatrix(int citiesCount) {
+        this.distances = new Float[citiesCount+2][citiesCount+2];
+    }
+
+    public double getDistances(int missionId, int centerId) {
+        //round the float to 3 deicimal plces
+        return Math.round(this.distances[missionId][centerId] * 10000.0) / 10000.0;
+        //return this.distances[missionId][centerId];
+    }
+
+    public void setDistances(int missionId, int centerId, Float distance) {
+        this.distances[missionId][centerId] = distance;
+    }
+}
